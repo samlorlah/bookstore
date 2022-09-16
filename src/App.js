b8d8bacbbd30f16c1329949a3fa74caad44f57df
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-09-16 13:47:11 +0100

    Modify App.css to style the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BooksContainer from './components/BooksContainer';
+import CategoriesContainer from './components/CategoriesContainer';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Book Store</h1>
+    <div className="app">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<BooksContainer />} />
+          <Route path="/categories" element={<CategoriesContainer />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
