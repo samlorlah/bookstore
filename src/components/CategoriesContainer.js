744efcaf35564d3bd1414f0217789e94a3de470b
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-09-20 10:07:21 +0100

    Check Categories Status using Redux

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const CategoriesContainer = () => (
-  <div className="categoriesContainer">
-    <button type="button">CHECK STATUS</button>
-  </div>
-);
+const CategoriesContainer = () => {
+  const dispatch = useDispatch();
+
+  const handleChangeStatus = () => dispatch(checkStatus());
+  const status = useSelector((state) => state.categoriesReducers);
+
+  return (
+    <div className="categoriesContainer">
+      <h2>{status}</h2>
+      <button type="button" onClick={handleChangeStatus}>CHECK STATUS</button>
+    </div>
+  );
+};
 
 export default CategoriesContainer;
