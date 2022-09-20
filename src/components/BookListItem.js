0a8e53deebb253bd6db39e825d0d1e96bbe69e88
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-09-20 09:43:46 +0100

    Use Redux to Add and Remove Books

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BookListItem = () => (
-  <li>
-    <div>
-      <p className="title">The Hunger Games</p>
-      <p className="author">Suzanne Collins</p>
-      <ul className="list-action-btns">
-        <li>Comments</li>
-        <li>Remove</li>
-        <li>Edit</li>
-      </ul>
-    </div>
-    <div className="completion-status">
-      <div className="chart" />
-      <p>64%</p>
-      <p>Completed</p>
-    </div>
-    <div className="chapter-section">
-      <p style={{ opacity: '0.5' }}>CURRENT CHAPTER</p>
-      <p>Chapter 17</p>
-      <button type="button">UPDATE PROGRESS</button>
-    </div>
-  </li>
-);
+const BookListItem = ({ id, title, author }) => {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      <div>
+        <p className="title">{title}</p>
+        <p className="author">{author}</p>
+        <ul className="list-action-btns">
+          <li>Comments</li>
+          <li>
+            <button type="button" onClick={() => dispatch(removeBook(id))} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+              Remove
+            </button>
+          </li>
+          <li>Edit</li>
+        </ul>
+      </div>
+      <div className="completion-status">
+        <div className="chart" />
+        <p>64%</p>
+        <p>Completed</p>
+      </div>
+      <div className="chapter-section">
+        <p style={{ opacity: '0.5' }}>CURRENT CHAPTER</p>
+        <p>Chapter 17</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+    </li>
+  );
+};
+
+BookListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
 
 export default BookListItem;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,8 +2,33 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const booksReducers = (state = [], action) => {
+const books = [
+  {
+    id: 1,
+    title: 'Book Reading',
+    author: 'Amadi Ojo',
+  },
+  {
+    id: 2,
+    title: 'Coming unto the mountain',
+    author: 'Prince Ethan',
+  },
+  {
+    id: 3,
+    title: 'Free from trial',
+    author: 'Jsuting wizkid',
+  },
+];
+
+const booksReducers = (state = books, action) => {
   switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        action.payload,
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
@@ -14,9 +39,9 @@ export const addBook = (payload) => ({
   payload,
 });
 
-export const removeBook = (payload) => ({
+export const removeBook = (id) => ({
   type: REMOVE_BOOK,
-  payload,
+  id,
 });
 
 export default booksReducers;
